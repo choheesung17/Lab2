274183cceb53024a66acbc02fab072d570850cac
heesungcho <heesung@snu.ac.kr>
2024-06-07 08:26:07 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -6,17 +6,23 @@ using namespace std;
 int N,M;
 vector<int> vec;
 
-void func(int cnt) {
+void func(int cnt, int last) {
+   if (cnt == M) {
+        for (int i=0; i<M; i++) {
+            cout << vec[i] << ' ';
+        }
+        cout << endl;
+        return;
+    }
 
-2023_18297 choheesung
-
-
-
-
-
+    for (int i=last+1; i<=N; i++) {
+        vec.push_back(i);     
+        func(cnt+1, i);     
+        vec.pop_back();       
+    }
 }
 
 int main() {
    cin >> N >> M;
-   func(0);
+   func(0, 0);
 }
